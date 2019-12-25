e344c0023d510e7e517d4a7002985c8c8280c313
yekaida <da183@163.com>
2019-12-25 13:46:06 +0800

    Site updated: 2019-12-25 13:46:04

---
+++ assets/DataConverter/strokePlace.lua
@@ -0,0 +1,199 @@
+local strokePlace = {
+	[1] = {
+		['ID'] = 1,
+		['name'] = 'AutoKey_1_name',
+		['cost'] = 1,
+		['probility'] = {
+			[1] = 80,
+			[2] = 10,
+			[6] = 10,
+		},
+		['image'] = 'Scenebg22',
+		['music'] = 'AudioJourneParkBgm.mp3',
+		['noshow'] = {
+			38,
+			39,
+			32,
+		},
+		['des'] = 'AutoKey_1_des',
+		['icon'] = 'StrokePark',
+		['opentime'] = {
+			{
+				['hour'] = 06,
+				['min'] = 00,
+			},
+			{
+				['hour'] = 21,
+				['min'] = 00,
+			},
+		},
+	},
+	[2] = {
+		['ID'] = 2,
+		['name'] = 'AutoKey_2_name',
+		['cost'] = 1,
+		['probility'] = {
+			[1] = 80,
+			[2] = 10,
+			[6] = 10,
+		},
+		['image'] = 'Scenebg32',
+		['music'] = 'AudioJourneyMovieBgm.mp3',
+		['noshow'] = {
+			24,
+			20,
+		},
+		['des'] = 'AutoKey_2_des',
+		['icon'] = 'StrokeFilmBase',
+		['opentime'] = {
+			{
+				['hour'] = 06,
+				['min'] = 00,
+			},
+			{
+				['hour'] = 21,
+				['min'] = 00,
+			},
+		},
+	},
+	[3] = {
+		['ID'] = 3,
+		['name'] = 'AutoKey_3_name',
+		['cost'] = 1,
+		['probility'] = {
+			[1] = 80,
+			[2] = 10,
+			[6] = 10,
+		},
+		['image'] = 'Scenebg21',
+		['music'] = 'AudioJourneShopBgm.mp3',
+		['noshow'] = {
+			38,
+			39,
+			40,
+			42,
+			43,
+			44,
+			21,
+			20,
+			24,
+			32,
+		},
+		['des'] = 'AutoKey_3_des',
+		['icon'] = 'StrokeShoppingStreet',
+		['opentime'] = {
+			{
+				['hour'] = 06,
+				['min'] = 00,
+			},
+			{
+				['hour'] = 21,
+				['min'] = 00,
+			},
+		},
+	},
+	[4] = {
+		['ID'] = 4,
+		['name'] = 'AutoKey_4_name',
+		['cost'] = 1,
+		['probility'] = {
+			[1] = 80,
+			[2] = 10,
+			[6] = 10,
+		},
+		['image'] = 'Scenebg15',
+		['music'] = 'AudioJourneyBarBgm.mp3',
+		['noshow'] = {
+			19,
+			20,
+			24,
+		},
+		['des'] = 'AutoKey_4_des',
+		['icon'] = 'StrokeBar',
+		['opentime'] = {
+			{
+				['hour'] = 06,
+				['min'] = 00,
+			},
+			{
+				['hour'] = 21,
+				['min'] = 00,
+			},
+		},
+	},
+	[5] = {
+		['ID'] = 5,
+		['name'] = 'AutoKey_5_name',
+		['cost'] = 1,
+		['probility'] = {
+			[1] = 5,
+			[6] = 65,
+			[5] = 30,
+		},
+		['image'] = 'Scenebg23',
+		['music'] = 'AudioJourneVillageBgm.mp3',
+		['noshow'] = {
+			38,
+			39,
+			40,
+			41,
+			42,
+			43,
+			44,
+			21,
+			20,
+			24,
+			32,
+		},
+		['des'] = 'AutoKey_5_des',
+		['icon'] = 'StrokeHolidayVillage',
+		['opentime'] = {
+			{
+				['hour'] = 06,
+				['min'] = 00,
+			},
+			{
+				['hour'] = 21,
+				['min'] = 00,
+			},
+		},
+	},
+	[6] = {
+		['ID'] = 6,
+		['name'] = 'AutoKey_6_name',
+		['cost'] = 1,
+		['probility'] = {
+			[1] = 5,
+			[6] = 65,
+			[5] = 30,
+		},
+		['image'] = 'Scenebg23',
+		['music'] = 'AudioJourneVillageBgm.mp3',
+		['noshow'] = {
+			38,
+			39,
+			40,
+			41,
+			42,
+			43,
+			44,
+			21,
+			20,
+			24,
+			32,
+		},
+		['des'] = 'AutoKey_6_des',
+		['icon'] = 'StrokeApartment',
+		['opentime'] = {
+			{
+				['hour'] = 06,
+				['min'] = 00,
+			},
+			{
+				['hour'] = 21,
+				['min'] = 00,
+			},
+		},
+	},
+}
+return strokePlace
